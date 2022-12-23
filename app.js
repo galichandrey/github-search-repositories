76d5ff51ec8de72ceca7439d7c4db0ba976bc525
galichandrey <galich.andrey@gmail.com>
2022-12-23 21:45:56 +0700

    Function style code. After first Mentor review

--- app.js
+++ app.js
@@ -1,117 +1,100 @@
-class View {
-  constructor(){
-    this.app = document.getElementById("app");
-    this.appInnerWrapper = this.createElement("div", "app__inner-wrapper");
+let app = document.getElementById("app");
+let appInnerWrapper = createElement("div", "app__inner-wrapper");
 
-    this.searchLine = this.createElement("div", "search-line");
-    this.searchInput = this.createElement("input", "search-input");
-    this.searchLine.append(this.searchInput);
+let searchLine = createElement("div", "search-line");
+let searchInput = createElement("input", "search-input");
+searchLine.append(searchInput);
 
-    this.autocompleteBox = this.createElement("ul", "autocomplete-box");
-    this.searchLine.append(this.autocompleteBox);
-    
-    this.btn = this.createElement("button", "btn");
-   
-    this.main = this.createElement("div", "main");
+let autocompleteBox = createElement("ul", "autocomplete-box");
+searchLine.append(autocompleteBox);
 
-    this.app.append(this.appInnerWrapper);
-    this.appInnerWrapper.append(this.searchLine);
-    this.app.append(this.main);
+let btn = createElement("button", "btn");
+
+let main = createElement("div", "main");
+
+app.append(appInnerWrapper);
+appInnerWrapper.append(searchLine);
+app.append(main);
+
+function createElement(elementTag, elementClass) {
+  const element = document.createElement(elementTag);
+  if(elementClass) {
+    element.classList.add(elementClass);
+  } 
+  return element;
+}
+
+function autocompleteShow(cardData) {
+  const cardElement = createElement('li', 'card-preview');
+  cardElement.addEventListener('click', () => {
+    autocompleteBox.innerHTML = "";
+    searchInput.value = "";
+
+    showRepoData(cardData);
+  });
+  cardElement.innerHTML = `<span>${cardData.name}</span>`;
+  autocompleteBox.append(cardElement);
+}
+
+function showRepoData(cardData) {
+  const repoCard = createElement("li", "repo-card");
+  const btn = createElement("div", "btn");
+
+  repoCard.addEventListener("click", (e) => {
+    autocompleteBox.innerHTML = "";
+  });
+
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    repoCard.remove();
+  })
+
+  repoCard.innerHTML = `<span class="repo-card__info"><span>Name: ${cardData.name}</span>
+  <span>Owner: ${cardData.owner.login}</span>
+  <span>Stars: ${cardData.stargazers_count}</span>
+  </span>`;
+  repoCard.append(btn);
+  main.prepend(repoCard);
+}
+
+searchInput.addEventListener('keyup', debounce(searchRepo.bind(this), 400));
+
+async function searchRepo(){
+  try {
+    if(searchInput.value) {
+      clearRepos();
+      await fetch(`https://api.github.com/search/repositories?per_page=5&q=${searchInput.value}`)
+      .then((response)=>{     
+        response.json().then((response) => {
+          response.items.forEach((repoData) => autocompleteShow(repoData));
+        })
+      });
+    } else {
+      clearRepos();        
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
   } 
 
-  createElement(elementTag, elementClass) {
-    const element = document.createElement(elementTag);
-    if(elementClass) {
-      element.classList.add(elementClass);
-    } 
-    return element;
-  }
-
-  autocompleteShow(userData) {
-    const cardElement = this.createElement('li', 'card-preview');
-    cardElement.addEventListener('click', () => {
-      this.autocompleteBox.innerHTML = "";
-      this.searchInput.value = "";
-
-      this.showRepoData(userData);
-    });
-    cardElement.innerHTML = `<span>${userData.name}</span>`;
-    this.autocompleteBox.append(cardElement);
-  }
-
-  showRepoData(userData) {
-    const repoCard = this.createElement("li", "repo-card");
-    const btn = this.createElement("div", "btn");
-
-    repoCard.addEventListener("click", (e) => {
-      this.autocompleteBox.innerHTML = "";
-    });
-
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      repoCard.remove();
-    })
-
-    repoCard.innerHTML = `<span class="repo-card__info"><span>Name: ${userData.name}</span>
-    <span>Owner: ${userData.owner.login}</span>
-    <span>Stars: ${userData.stargazers_count}</span>
-    </span>`;
-    repoCard.append(btn);
-    this.main.prepend(repoCard);
-  }
-
 }
 
-class Search {
+function clearRepos() {
+  autocompleteBox.innerHTML = "";
+}
 
-  constructor(view) {
-    this.view = view;
-
-    this.view.searchInput.addEventListener('keyup', this.debounce(this.searchRepo.bind(this), 400));
+function debounce (fn, debounceTime = 400) {
+  let timeout;  
   
-  }
+  return function () {
+    const context = this;
+    const args = arguments;
 
-  async searchRepo(){
-    try {
-      if(this.view.searchInput.value) {
-        this.clearRepos();
-        await fetch(`https://api.github.com/search/repositories?per_page=5&q=${this.view.searchInput.value}`)
-        .then((response)=>{     
-          response.json().then((response) => {
-            response.items.forEach((repoData) => this.view.autocompleteShow(repoData));
-          })
-        });
-      } else {
-        this.clearRepos();        
-      }
-    } catch (error) {
-      console.log(`Error: ${error}`);
-    } 
-
-  }
-
-  clearRepos() {
-    this.view.autocompleteBox.innerHTML = "";
-  }
-
-  debounce (fn, debounceTime = 400) {
-    let timeout;  
-    
-    return function () {
-      const context = this;
-      const args = arguments;
-  
-      function callFn(){
-        return fn.apply(context, args);
-      }
-  
-      clearTimeout(timeout);
-  
-      timeout = setTimeout(callFn, debounceTime);
+    function callFn(){
+      return fn.apply(context, args);
     }
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(callFn, debounceTime);
   }
-
-
 }
-
-const search = new Search(new View());
